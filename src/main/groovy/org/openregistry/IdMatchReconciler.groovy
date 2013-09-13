d04a88b81abdce6eccef6274d6effdf27bcb18c2
Jonathan Johnson <jj@scaldingspoon.com>
2013-09-13 15:28:55 -0500

    [#5] initial go at reconciler. need a more fleshed out id match engine to do further testing.

--- src/main/groovy/org/openregistry/IdMatchReconciler.groovy
+++ src/main/groovy/org/openregistry/IdMatchReconciler.groovy
@@ -2,17 +2,38 @@ package org.openregistry
 
 import org.openregistry.core.domain.sor.ReconciliationCriteria
 import org.openregistry.core.domain.sor.SorPerson
+import org.openregistry.core.repository.PersonRepository
+import org.openregistry.core.repository.ReferenceRepository
 import org.openregistry.core.service.reconciliation.Reconciler
 import org.openregistry.core.service.reconciliation.ReconciliationResult
+import org.openregistry.core.service.reconciliation.ReconciliationResultImpl
+import org.openregistry.identifier.IdMatchService
+import org.openregistry.identifier.IdMatchServiceResponse
 
 class IdMatchReconciler implements Reconciler {
+    IdMatchService idMatchService
+    PersonRepository personRepository
+    ReferenceRepository referenceRepository
+
     @Override
     ReconciliationResult reconcile(ReconciliationCriteria reconciliationCriteria) {
-        throw new UnsupportedOperationException("this hasn't been implemented yet")
+        def idMatchServiceResponse = idMatchService.getIdMatchServiceResponse(reconciliationCriteria.sorPerson)
+        switch (idMatchServiceResponse.status) {
+            case IdMatchServiceResponse.Status.OK:
+                return new ReconciliationResultImpl(ReconciliationResult.ReconciliationType.EXACT, [personRepository.findByIdentifier("IDMATCH", idMatchServiceResponse.referenceId)])
+            case IdMatchServiceResponse.Status.MULTIPLE_CHOICES:
+                return new ReconciliationResultImpl(
+                        ReconciliationResult.ReconciliationType.MAYBE,
+                        idMatchServiceResponse.fullResponse.candidates.referenceId.collect {String it -> personRepository.findByIdentifier("IDMATCH", it)}
+                )
+            default:
+                return new ReconciliationResultImpl(ReconciliationResult.ReconciliationType.NONE, [])
+        }
     }
 
     @Override
     boolean reconcilesToSamePerson(SorPerson sorPerson) {
-        throw new UnsupportedOperationException("this hasn't been implemented yet")
+        //TODO: implement this
+        return true
     }
 }
